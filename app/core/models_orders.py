a5335f1b787890081b228e153c1e123d176a66bb
vkhehay <v.khehay@gmail.com>
2026-03-22 18:12:40 +0200

    created order table

--- app/core/models_orders.py
+++ app/core/models_orders.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 
 class Order(Base):
-    promocode: Mapped[str]
+    promocode: Mapped[str | None]
     created_at: Mapped[datetime] = mapped_column(
         DateTime,
         server_default=func.now(),
